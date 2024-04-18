612d1819ad61ffb6420e3fe5d129bba44cfdf38e
worldbeater2 <realdudization@gmail.com>
2024-04-18 12:13:49 +0000

    Banner added

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -6,7 +6,17 @@ import Footer from '../components/Footer'
 const Career = () => {
     return (
         <div><Navbar /> 
-        <Footer/>
+          <div className="hero min-h-[320px]" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1591800076009-a4a2c5582f7d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)'}}>
+  <div className="hero-overlay bg-opacity-55"></div>
+  <div className="hero-content text-start text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-7xl font-light tracking-wider font-navFont">Join Us</h1>
+    
+  
+    </div>
+  </div>
+</div>
+        
         
         </div>
       )
