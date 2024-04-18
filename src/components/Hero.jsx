0f154771a0017afddb205fe170df27fd6c2b5e8d
worldbeater2 <realdudization@gmail.com>
2024-04-18 12:08:53 +0000

    redo of homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,8 @@
 // import React, { useState, useEffect } from 'react';
 
 import React, { useState, useEffect } from 'react';
-
+// url: 'https://images.unsplash.com/photo-1518897761968-5a3544c369b5?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+// url: 'https://images.unsplash.com/photo-1509130298739-651801c76e96?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 const images = [
   {
     url: 'https://images.unsplash.com/photo-1711075781376-bc5107736730?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -9,12 +10,12 @@ const images = [
     text: 'Expert Legal Counsel for Corporate and Commercial Ventures'
   },
   {
-    url: 'https://images.unsplash.com/photo-1518897761968-5a3544c369b5?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    url: 'https://images.unsplash.com/photo-1531591022136-eb8b0da1e6d0?q=80&w=2912&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     heading: 'Your Path to Corporate Clarity',
     text: 'Understanding and Resolving Corporate Legal Challenges'
   },
   {
-    url: 'https://images.unsplash.com/photo-1509130298739-651801c76e96?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    url: 'https://images.unsplash.com/photo-1469719847081-4757697d117a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     // heading: 'Where Tradition Meets Innovation',
     // text: 'Guiding Businesses with Timeless Expertise and Modern Solutions'
     heading: 'Unlock Success with Black Swan',
@@ -47,7 +48,7 @@ const Hero = () => {
 
   return (
     <div
-      className="hero min-h-[745px] w-full relative overflow-hidden bg-cover font-navFont hover:bg-blend-darken"
+      className="hero min-h-screen w-full relative overflow-hidden bg-cover font-navFont hover:bg-blend-normal"
       style={{
         backgroundImage: `url(${images[currentImageIndex].url})`,
         backgroundPosition: 'center',
@@ -55,7 +56,7 @@ const Hero = () => {
         transition: 'background-image 2s ease-in-out'
       }}
     >
-      <div className="hero-overlay bg-opacity-26 "></div>
+      <div className="hero-overlay bg-opacity-60 "></div>
       <div className="hero-content  text-start text-neutral-content">
         <div className="max-w-5xl">
           <h1 className="mb-5 text-7xl font-light tracking-wider">{currentHeading}</h1>
