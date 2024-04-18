9f8b27e6ac2f40d7bc3148d0f7023cd9cfc13b16
worldbeater2 <realdudization@gmail.com>
2024-04-18 12:15:55 +0000

    career page fix

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -6,7 +6,7 @@ import Footer from '../components/Footer'
 const Career = () => {
     return (
         <div><Navbar /> 
-          <div className="hero min-h-[320px]" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1591800076009-a4a2c5582f7d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)'}}>
+        <div className="hero min-h-[320px]" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1591800076009-a4a2c5582f7d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)'}}>
   <div className="hero-overlay bg-opacity-55"></div>
   <div className="hero-content text-start text-neutral-content">
     <div className="max-w-md">
@@ -16,7 +16,7 @@ const Career = () => {
     </div>
   </div>
 </div>
-        
+        <Footer/>
         
         </div>
       )
