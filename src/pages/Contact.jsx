1a7ca86c94d48117b17da8de8dfe565c24da6455
worldbeater2 <realdudization@gmail.com>
2024-06-02 05:05:24 +0000

    contact us begins

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     return (
         <main><Navbar /> 
         
-        <div className="hero min-h-[320px]" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1591800076009-a4a2c5582f7d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)'}}>
+        {/* <div className="hero min-h-[320px]" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1591800076009-a4a2c5582f7d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)'}}>
   <div className="hero-overlay bg-opacity-55"></div>
   <div className="hero-content text-start text-neutral-content">
     <div className="max-w-md">
@@ -16,16 +16,16 @@ const Contact = () => {
   
     </div>
   </div>
-</div>
-<section className='flex p-5  mx-auto'>
+</div> */}
+<section className='flex p-10 mt-10  mx-auto'>
 <div className=' container w-96 h-96 ml-10 mt-10' >
 <label className="input input-bordered flex items-center gap-2 mb-2" >
   Name
-  <input type="text" className="grow" placeholder="Daisy" />
+  <input type="text" className="grow" placeholder="Name" />
 </label>
 <label className="input input-bordered flex items-center gap-2">
   Email
-  <input type="text" className="grow" placeholder="daisy@site.com" />
+  <input type="text" className="grow" placeholder="Your Email" />
 </label>
 
 
@@ -35,7 +35,7 @@ const Contact = () => {
           className="mx-auto text-center"
         >
           <div className="container font-navFont leading-tight w-[1000px] font-semibold text-black tracking-tighter text-6xl ">
-            Discover Our Wide Range of Practice Areas and Expertise
+          Get in Touch with Us
           </div>
         </div>
 
