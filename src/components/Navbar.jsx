de44b88ec6e24462e17ad8be9ad8b2aaff4149b2
worldbeater2 <realdudization@gmail.com>
2024-06-07 21:28:51 +0000

    first page mobile view done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="navbar bg-gray-100 mx-auto bg-transparent font-navFont text-xl ">
-      <div className="navbar-start">
-        <div className="">
+    <nav className="bg-gray-100 font-navFont ">
+      {/* Mobile view */}
+      <div className="flex justify-between items-center w-full lg:hidden px-4 py-2">
+        <Link to={"/"} className="flex justify-center w-full">
+          <img className="w-32 p-2.5 stroke-2" src="/Vector (1).svg" alt="Logo" />
+        </Link>
+        <button className="p-2 focus:outline-none" onClick={toggleMenu}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
+      </div>
+
+      {isMenuOpen && (
+        <div className="lg:hidden flex flex-col mr-10 pb-2 items-center mt-4 space-y-2">
+          <Link to={"/about"} onClick={toggleMenu} className="text-sm">About Us</Link>
+          <Link to={"/expertise"} onClick={toggleMenu} className="text-sm">Expertise</Link>
+          <Link to={"/people"} onClick={toggleMenu} className="text-sm">People</Link>
+          <Link to={"/insights"} onClick={toggleMenu} className="text-sm">Insights</Link>
+          <Link to={"/contact"} onClick={toggleMenu} className="text-sm">Contact</Link>
+          <Link to={"/career"} onClick={toggleMenu} className="text-sm">Career</Link>
+          <Link to={"/products"} onClick={toggleMenu} className="text-sm">Products</Link>
+        </div>
+      )}
+
+      {/* Larger screens view */}
+      <div className="hidden lg:flex lg:items-center lg:justify-between w-full px-4 text-sm md:text-base lg:text-lg xl:text-xl">
+        <div className="navbar-start">
           <ul className="flex">
             <li className="ml-7 mr-7">
-              <Link to={"/about"}>About Us </Link>
+              <Link to={"/about"}>About Us</Link>
             </li>
             <li className="mr-7">
               <Link to={"/expertise"}>Expertise</Link>
@@ -21,44 +63,42 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-      </div>
 
-      <div className="navbar-center">
-        <Link to={"/"} className="">
-          <img className="w-[250px] p-2.5 stroke-2" src="/Vector (1).svg"></img>
-        </Link>
-      </div>
-
-      <div className="navbar-end">
-        <div className="">
-          <ul className="flex items-center">
-            <li className="ml-0 mr-7">
-              <Link to={"/contact"}>Contact</Link>
-            </li>
-            <li className="mr-7">
-              <Link to={"/career"}>Career</Link>
-            </li>
-            <li className="mr-7">
-              <Link to={"/products"}>Products</Link>
-            </li>
-            <button className="btn btn-ghost btn-circle mr-7">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-            </button>
-          </ul>
+        <div className="navbar-center">
+          <Link to={"/"}>
+            <img className="  md:w-[100px] lg:w-[180px] xl:w-[180px] p-2.5 stroke-2" src="/Vector (1).svg" alt="Logo" />
+          </Link>
         </div>
+
+        <div className="navbar-end hidden lg:flex items-center">
+        <ul className="flex items-center">
+          <li className="ml-0 mr-7">
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+          <li className="mr-7">
+            <Link to={"/career"}>Career</Link>
+          </li>
+          <li className="mr-7">
+            <Link to={"/products"}>Products</Link>
+          </li>
+          <button className="btn btn-ghost btn-circle mr-7">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+        </ul>
+      </div>
       </div>
     </nav>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,28 +4,20 @@ import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import { Link } from "react-router-dom";
 
-
-
-
 const Homepage = () => {
-
   return [
-    <main className="bg-white text-black brightness-110" >
+    <main className="bg-white text-black brightness-110">
       <section className="">
         <Navbar />
         <Hero />
-        <section className="w-screen" >
-          <div className="mt-10 ml-16 p-10 max-w-[1000px] text-justify scroll-smooth">
-            <div
-              className="mx-auto ml-5 font-navFont text-3xl mb-10 font-medium"
-              
-            >
+
+        <section className="w-screen">
+          <div className="md:mt-10 mt-5 p-4  md:ml-16 md:p-10 md:max-w-[1000px]  text-justify scroll-smooth">
+            <div className="mx-auto ml-5 font-navFont text-3xl md:mb-10 mb-5 font-medium flex justify-center md:justify-start ">
               About Us{" "}
             </div>
-            <div
-              className="mx-auto ml-5 font-navFont tracking-wider"
-       
-            >
+
+            <div className="mx-auto md:ml-5 ml-1 md:p-0 p-3 font-navFont tracking-wider">
               {" "}
               Black Swan is a modern law firm dedicated to providing exceptional
               legal services across a wide range of expertise. Our team of
@@ -41,146 +33,129 @@ const Homepage = () => {
               through legal challenges with professionalism, expertise, and a
               fresh approach to innovation.{" "}
             </div>
-            <div className=" mx-5 ml-5 font-navFont ">
+            <div className=" mx-5 ml-5 md:mx-5 md:ml-5 flex flex-row justify-center md:justify-start font-navFont ">
               {" "}
               <Link to="/about">
-              <button
-                className="btn file: text-black btn-outline px-10 mt-10 "
-                
-              >
-                Know More
-              </button>
-
+                <button className="btn file: text-black btn-outline px-10 mt-10 ">
+                  Know More
+                </button>
               </Link>
-           
             </div>
           </div>
         </section>
-        <section >
-        <div className=" ml-16 p-10 max-w-[1000px] text-justify scroll-smooth ">
-          <div
-            className="mx-auto ml-5 font-navFont text-3xl font-medium mb-10 "
-           
-          >
-            Practice Areas
-          </div>
-          <div
-            className="mx-auto ml-5 font-navFont tracking-wider"
-            
-          >
-            Our team of seasoned professionals is dedicated to providing
-            top-notch legal services across various specialties. With a focus on
-            excellence and a commitment to delivering results, we strive to
-            exceed our clients' expectations in every case. <br /> <br />{" "}
-            Explore our practice areas below to discover how Black Swan can
-            assist you in navigating the complexities of the legal landscape
-            with confidence and clarity.
-          </div>
-        </div>
-        <div
-          className=" container grid grid-cols-4 mt-1 mx-auto p-7 gap-x-3 font-navFont"
-          
-        >
-          <div className="card w-75 bg-base-100 shadow-xl image-full hover:-translate-y-6 transition-all duration-100 hover:brightness-200">
-            <figure>
-              <img
-                className="h-full w-full "
-                src="https://images.unsplash.com/photo-1554469384-e58fac16e23a?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Shoes"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title text-center">
-                Corporate & Commercial Law
-              </h2>
+
+        <section>
+          <div className=" md:ml-16 md:p-10  md:max-w-[1000px] text-justify scroll-smooth ">
+            <div className="mx-auto ml-5 font-navFont text-3xl font-medium md:mt-0 mt-5 mb-7 flex flex-row justify-center md:justify-start">
+              Practice Areas
+            </div>
+            <div className="mx-auto md:ml-5 ml-1 md:p-0 p-4 font-navFont tracking-wider">
+              Our team of seasoned professionals is dedicated to providing
+              top-notch legal services across various specialties. With a focus
+              on excellence and a commitment to delivering results, we strive to
+              exceed our clients' expectations in every case. <br /> <br />{" "}
+              Explore our practice areas below to discover how Black Swan can
+              assist you in navigating the complexities of the legal landscape
+              with confidence and clarity.
             </div>
           </div>
 
-          <div className="card w-75 bg-base-100 shadow-xl image-full hover:-translate-y-6 transition-all duration-100 hover:brightness-200">
-            <figure>
-              <img
-                src="https://plus.unsplash.com/premium_photo-1694164360654-f5e83fac9452?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="corporate"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title text-center">Disputes & Litigation</h2>
-            </div>
-          </div>
-
-          <div className="card w-75 bg-base-100 shadow-xl image-full hover:-translate-y-6 transition-all duration-100 hover:brightness-200">
-            <figure>
-              <img
-                src="https://images.unsplash.com/photo-1531169509526-f8f1fdaa4a67?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Abstract Technology"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title text-center">
-                Intellectual Property & Technology
-              </h2>
-            </div>
-          </div>
-          <div className="card w-75 bg-base-100 shadow-xl image-full hover:-translate-y-6 transition-all duration-100 hover:brightness-200 ">
-            <figure>
-              <img
-                src="https://images.unsplash.com/photo-1572276037952-478cead56982?q=80&w=2528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D3"
-                alt="building"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title text-center">
-                Financial Institutions & Capital Markets
-              </h2>
-            </div>
-          </div>
-        </div>
-        ,
-        <div className="ml-16 container text-justify mx-auto mb-10 font-navFont mt-4">
-          <div className="flex justify-center ">
-          <Link to="/expertise">
-          <div className="flex text-black  px-9 duration-150 hover:animate-bounce  hover:border-b-2 hover:border-swanred hover:cursor-pointer border-black block">
-              Visit our Expertise Page{" "}
-              <svg
-                className="w-4 ml-3"
-                dataSlot="icon"
-                fill="none"
-                strokeWidth={2}
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+          <div className=" lg:container lg:grid lg:grid-cols-4 lg:mt-1 lg:mx-auto p-7 gap-x-3 font-navFont hidden">
+            <div className="card w-75 bg-base-100 shadow-xl image-full hover:-translate-y-6 transition-all duration-100 hover:brightness-200">
+              <figure>
+                <img
+                  className="h-full w-full "
+                  src="https://images.unsplash.com/photo-1554469384-e58fac16e23a?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="Shoes"
                 />
-              </svg>
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-center">
+                  Corporate & Commercial Law
+                </h2>
+              </div>
             </div>
 
-          </Link>
-         
+            <div className="card w-75 bg-base-100 shadow-xl image-full hover:-translate-y-6 transition-all duration-100 hover:brightness-200">
+              <figure>
+                <img
+                  src="https://plus.unsplash.com/premium_photo-1694164360654-f5e83fac9452?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="corporate"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-center">
+                  Disputes & Litigation
+                </h2>
+              </div>
+            </div>
+
+            <div className="card w-75 bg-base-100 shadow-xl image-full hover:-translate-y-6 transition-all duration-100 hover:brightness-200">
+              <figure>
+                <img
+                  src="https://images.unsplash.com/photo-1531169509526-f8f1fdaa4a67?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="Abstract Technology"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-center">
+                  Intellectual Property & Technology
+                </h2>
+              </div>
+            </div>
+            <div className="card w-75 bg-base-100 shadow-xl image-full hover:-translate-y-6 transition-all duration-100 hover:brightness-200 ">
+              <figure>
+                <img
+                  src="https://images.unsplash.com/photo-1572276037952-478cead56982?q=80&w=2528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D3"
+                  alt="building"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-center">
+                  Financial Institutions & Capital Markets
+                </h2>
+              </div>
+            </div>
           </div>
-        </div>
+
+          <div className="md:ml-16 container text-justify md:mb-10 font-navFont md:mt-4 flex mt-3 justify-center">
+            <div className="  ">
+              <Link to="/expertise">
+                <div className="flex text-black  px-9 duration-150 hover:animate-bounce  hover:border-b-2 hover:border-swanred hover:cursor-pointer border-black block">
+                  Visit our Expertise Page{" "}
+                  <svg
+                    className="w-4 ml-3"
+                    dataSlot="icon"
+                    fill="none"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                    />
+                  </svg>
+                </div>
+              </Link>
+            </div>
+          </div>
         </section>
 
-        <div className=" ml-16 p-10 max-w-[1000px] text-justify scroll-smooth ">
-          <div
-            className="mx-auto  font-navFont text-3xl mb-12 text-black font-medium" 
-            
-          >
+        <div className="  p-10 max-w-[1000px] md:text-justify scroll-smooth ">
+          <div className="mx-auto ml-5 font-navFont text-3xl font-medium md:mt-0 mt-5 mb-7 md:mb-10 flex flex-row justify-center md:justify-start">
             Latest Insights
           </div>
-          <div
-            className="grid grid-cols-6 gap-x-7 font-navFont "
-            
-          >
+
+          <div className="md:grid md:grid-cols-6 md:gap-x-7 flex flex-col font-navFont ">
             <div className="col-span-4 ">
-              <div className="card lg:card-side bg-base-100  rounded-md cursor-pointer  duration-700 hover:animate-pulse p-2">
+              <div className="card md:card-side bg-base-100  rounded-md cursor-pointer  duration-700 hover:animate-pulse p-2">
                 <figure>
                   <img
-                    className="overflow-hidden h-[350px] w-[400px] "
+                    className="overflow-hidden md:h-[350px] md:w-[400px] "
                     src="https://images.unsplash.com/photo-1568025848823-86404cd04ad1?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt="building"
                   />
@@ -191,25 +166,24 @@ const Homepage = () => {
                   </h2>
                   <p className="">04 April,2024</p>
                   <div className="card-actions justify-start">
-                  <Link to="/insights" >
-                  <button className="btn btn-outline px-10 hover:bg-black  hover:text-white">
-                      Read more
-                    </button>
-
-                  </Link>
-             
+                    <Link to="/insights">
+            
+                      <button className="md:btn md:btn-outline md:px-10 md:hover:bg-black  md:hover:text-white hidden">
+                        Read more
+                      </button>
+                    </Link>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div className="col-span-2 w-[600px] rounded-lg mx-auto font-navFont p-2">
+            <div className="col-span-2 md:w-[600px] rounded-lg mx-auto font-navFont p-2">
               <a href="/">
                 <div className="flex flex-col border-l py-5 px-5 hover:border-swanred hover:animate-pulse duration-700">
                   <div className="flex ">
                     <img
                       src="https://images.unsplash.com/photo-1670159270770-9a455ba607d1?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                      className="overflow-hidden object-fill rounded-lg w-[300px] "
+                      className="overflow-hidden object-cover rounded-lg w-[300px] "
                     ></img>
                     <div className="p-5 flex-row">
                       <h1 className="font-semibold ">
@@ -226,7 +200,7 @@ const Homepage = () => {
                   <div className="flex ">
                     <img
                       src="https://images.unsplash.com/photo-1505424297051-c3ad50b055ae?q=80&w=2275&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                      className="overflow-hidden  image-full object-cover rounded-lg w-[300px] h-[145px]"
+                      className="overflow-hidden image-full object-cover rounded-lg w-[300px] h-[145px]"
                     ></img>
                     <div className="p-5 flex-row">
                       <h1 className="font-semibold ">
@@ -241,32 +215,30 @@ const Homepage = () => {
             </div>
           </div>
         </div>
-        ,
-        <div className="ml-16 container text-justify mx-auto mb-16 font-navFont mt-4">
-          <div className="flex justify-center ">
-          <Link to="/insights" >
-          <div className="flex text-black  px-9 duration-150 hover:animate-bounce  hover:border-b-2 hover:border-swanred hover:cursor-pointer border-black block">
-              More Insights{" "}
-              <svg
-                className="w-4 ml-3"
-                dataSlot="icon"
-                fill="none"
-                strokeWidth={2}
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                />
-              </svg>
-            </div>
 
-          </Link>
-          
+        <div className="md:ml-16 container text-justify md:mb-10 mb-5 -mt-5 font-navFont md:mt-4 flex  justify-center">
+          <div className="flex justify-center ">
+            <Link to="/insights">
+              <div className="flex text-black  px-9 duration-150 hover:animate-bounce  hover:border-b-2 hover:border-swanred hover:cursor-pointer border-black block">
+                More Insights{" "}
+                <svg
+                  className="w-4 ml-3"
+                  dataSlot="icon"
+                  fill="none"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                  />
+                </svg>
+              </div>
+            </Link>
           </div>
         </div>
       </section>
@@ -275,17 +247,14 @@ const Homepage = () => {
         <div className="mb-5 ">
           Keep up to date with events and updates, now and in the future.
         </div>
-        <Link to="/insights" >
-        <button  className="btn btn-neutral font-light bg-swanblack text-xl border-swanblack mb-5 " 
-              >
-          SUBSCRIBE
-        </button >
-
+        <Link to="/insights">
+          <button className="btn btn-neutral font-light bg-swanblack text-xl border-swanblack mb-5 ">
+            SUBSCRIBE
+          </button>
         </Link>
-       
       </div>
       <Footer />
-    </main>
+    </main>,
   ];
 };
 
